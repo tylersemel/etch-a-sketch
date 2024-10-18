8a9787341cc5a0f377c2968e52a7298f7069e818
tylersemel <tylersemel@gmail.com>
2024-10-18 13:27:20 -0400

    Create container items color change functionality

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-const GRID_SIZE = 16;
+const INIT_GRID_SIZE = 16;
 const container = document.querySelector(".container");
+const SIZE = 480;
 
-
-function createGrid() {
-    for (let i = 1; i <= GRID_SIZE * GRID_SIZE; i++) {
+function createGrid(gridSize) {
+    let gridSpaces = [];
+    for (let i = 1; i <= gridSize * gridSize; i++) {
         let gridSpace = document.createElement("div");
+        gridSpace.classList.add(`${i}`);
+        let width = (SIZE / gridSize).toString();
+        gridSpace.setAttribute("style", `width:${width}px`)
+        gridSpaces[i - 1] = gridSpace;
         container.appendChild(gridSpace);
     }
 
+    console.log("space: ", gridSpaces[0].style.width);
+
 }
 //711 729
-createGrid();
+createGrid(INIT_GRID_SIZE);
+
+function getRandomColorVal() {
+    let min = 0;
+    let max = 255
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+container.addEventListener("mouseover", (e) => {
+    console.log("test", e.target);
+    let color = [getRandomColorVal(), getRandomColorVal(), getRandomColorVal()];
+    e.target.style.backgroundColor = `rgb(${color})`;
+});
