0c91e9c7475dd186fa068ac8f359f6deadceb99c
tylersemel <tylersemel@gmail.com>
2024-10-18 14:39:03 -0400

    Add user specified grid size

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const INIT_GRID_SIZE = 16;
 const container = document.querySelector(".container");
 const SIZE = 480;
 const btn = document.querySelector("button");
+let gridSpaces = [];
 
 function createGrid(gridSize) {
-    let gridSpaces = [];
     for (let i = 1; i <= gridSize * gridSize; i++) {
         let gridSpace = document.createElement("div");
         gridSpace.classList.add(`${i}`);
@@ -27,11 +27,22 @@ function getRandomColorVal() {
 }
 
 container.addEventListener("mouseover", (e) => {
-    console.log("test", e.target);
-    let color = [10, getRandomColorVal(), getRandomColorVal()];
+    let color = [getRandomColorVal(), getRandomColorVal(), getRandomColorVal()];
     e.target.style.backgroundColor = `rgb(${color})`;
 });
 
 btn.addEventListener("click", () => {
     console.log("test");
+    let userGridSize = prompt("Enter a new grid size (Max 100)", 16);
+
+    if (userGridSize < 1 || userGridSize > 100) {
+        alert("Try again!");
+        return;
+    }
+
+    for (let i = 0; i < gridSpaces.length; i++) {
+        gridSpaces[i].remove();
+    }
+
+    createGrid(userGridSize);
 });
